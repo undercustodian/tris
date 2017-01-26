0cfc9e94e6f230608651f211d8a182c35550ccb8
undercustodian <undercustodian@gmail.com>
2017-01-26 17:17:47 +0100

    riscrittto tris.js come me la sento

--- tris.js
+++ tris.js
@@ -1,48 +1,95 @@
-var grid=[0,0,0,0,0,0,0,0,0,0];
-var cord=[[0,0],[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]];
-var rig=[0,0,0,0];
-var col=[0,0,0,0];
-var diag=[0,0,0];
-var turno=1;
-var numMosse=0;
-resultString="";
+var matr; //matrice del tris
+var giocatore;
+var turno;
+var numMosse; // numero delle mosse
+var resultString; // risultato
+var col; // numeri determinante per le tre colonna
+var rig; // numeri determinante per le tre riga
+var dig; // numeri determinante per le due diagonali
+var det; // determinate
+info(); // informazioni di debug
+start();
 
-function printData(cellId){
-	
+
+// porta allo stato iniziale
+function start(){
+	console.log("**************  START ***************");
+	matr=[[0,0,0],[0,0,0],[0,0,0]];
+	giocatore=1;
+	turno=1;
+	numMosse=0;
+	resultString="";
+	col=[0,0,0];
+	rig=[0,0,0];
+	dig=[0,0];
+	det=0;
+}
+
+// porta alle condizioni iniziali
+function restart(){
+	start();
+	for(id=0;id<9;id++) window.document.getElementById(id).innerHTML="";
+}
+
+// funzione chiamata dall'evento onclick sulla cella
+function printData(numRig, numCol){
 	// input
-	var tdId=document.getElementById(cellId);
-	if(grid[cellId]!=0){
-		return;
-	}
-	numMosse++;
-	grid[cellId]=turno;
+	var tdId=document.getElementById(3*numRig+numCol);
+	if(matr[numRig][numCol]!=0) return; // valore casella già assegnato
+	numMosse++
+	console.log("================  " +numMosse+ "  ================");
+	console.log("coordinate riga  colonna : ("+numRig+","+numCol+")");
 	if(turno==1){
 		tdId.innerHTML="X";
-		turno=4;
+		turno=-1;
 	}else{
 		tdId.innerHTML="O";
 		turno=1;
 	}
+	matr[numRig][numCol]=giocatore;
 	
-	// main
-	rig[cord[cellId][0]]+=grid[cellId];
-	col[cord[cellId][1]]+=grid[cellId];
-	if(rig[cord[cellId][0]]==col[cord[cellId][1]]){
-		diag[0]+=grid[cellId];
-	}
-	if((rig[cord[cellId][0]]+col[cord[cellId][1]])==4){
-		diag[1]+=grid[cellId];
-	}
-	cand=Math.max(rig[cord[cellId][0]], col[cord[cellId][1]],diag[0], diag[1]);
-	if(cand==12 || cand==3){
-		if(grid[cellId]==1){
-			resultString="Ha vinto X";
-		}else{
-			resultString="Ha vinto O";
-		}
-	}
-	if(numMosse==9) resultString="Patta";
-	
-	// output	
-	if(resultString) alert(resultString);
+	//main
+	console.log("giocatore: " + giocatore);
+	// calcolo numeri determinante
+	rig[numRig]+=giocatore; 
+	col[numCol]+=giocatore;
+	if(numRig==numCol) dig[0]+=giocatore;
+	if(numRig+numCol==2) dig[1]+=giocatore;
+	// calcolo determinante
+	det=Math.max(0,Math.max(rig[numRig],col[numCol],dig[0],dig[1])-2) +
+	Math.min(0,Math.min(rig[numRig],col[numCol],dig[0],dig[1])+2);
+	console.log("numeri determinante:");
+	console.log("\trighe:     ("+rig[0]+","+rig[1]+","+rig[2]+")");
+	console.log("\tcolonne:   ("+col[0]+","+col[1]+","+col[2]+")");
+	console.log("\tdiagonali: ("+dig[0]+","+dig[1]+")");
+	console.log("determinante: "+ det);
+	// determina risultato
+	if(det>0) resultString="Ha vinto X";
+	if(det<0) resultString="Ha vinto O";
+	if(det==0) if(numMosse<9){
+		giocatore=turno;
+		return;
+	}else resultString="Patta"; // finito le mosse
+	giocatore = turno;
+	console.log("risultato: " + resultString);
+
+	// output
+	alert(resultString);
+	console.log("           ***   END  ***");
+	restart();
+}
+
+// informazione di log
+function info(){
+	console.log("  ********************  info ********************  ");
+	console.log("==============   numero delle mosse  ==============");
+	console.log("coordinate riga  colonna : della cella selezionata");
+	console.log("giocatore: 1 giocatore X, -1 giocatore O");
+	console.log("numeri determinante compresi fra -3 e 3:");
+	console.log("\tcalcolati per ogni riga, colona, diagonale;");
+	console.log("\t3 vince X , -3 vince O, altrimenti nessuno vince;");
+	console.log("determinante ha valore -1,0,1:");
+	console.log("\t1 vince X , -1 vince O, altrimenti nessuno vince;");
+	console.log("risultato: dice chi ha vinto o se è patta");
+	console.log("  ***********************************************  ");
 }
